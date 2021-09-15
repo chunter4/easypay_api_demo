c91d3e18ec2badd72639319eefa6cd814d17110b
Chris Hunter <Christopher.Hunter@coxautoinc.com>
2021-09-14 19:28:38 -0700

    add test to post application end point

--- EasyPay_Project/api_nunit_demo.cs
+++ EasyPay_Project/api_nunit_demo.cs
@@ -101,6 +101,7 @@ namespace EasyPay_Project
         [TestCase("/api/Application", "{ \"applicationId\": 123, \"name\": \"test\", \"age\": \"31\", \"amount\": 18.0 }", HttpStatusCode.BadRequest, "The field Age must be between 18 and 30.")]
         [TestCase("/api/Application", "{ \"applicationId\": 123, \"name\": \"test\", \"age\": \"18\", \"amount\": \"test\" }", HttpStatusCode.BadRequest, "The JSON value could not be converted to System.Decimal. Path: $.amount")]
         [TestCase("/api/Application", "{ \"applicationId\": 123, \"name\": 123, \"age\": \"18\", \"amount\": 18.0 }", HttpStatusCode.BadRequest, "The JSON value could not be converted to System.String. Path: $.name")]
+        [TestCase("/api/Application", "{ \"applicationId\": 123, \"name\": \"test\", \"age\": 18, \"amount\": 18.0 }", HttpStatusCode.BadRequest, "The JSON value could not be converted to System.String. Path: $.age")]
         public void Post_Application(string resource, string body, HttpStatusCode expReturnCode, string expErrorMsg)
         {
             postAPI(resource, body, expReturnCode, expErrorMsg);
